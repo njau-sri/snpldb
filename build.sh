1174003163c5825c8e476a292c15686f81026e5f
Jianbo He <hjbxyz@gmail.com>
2017-10-12 09:37:02 +0800

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-g++ *.cpp -o snpldb -s -O2 -std=c++11 -static
+rm -rf snpldb-$1
+mkdir snpldb-$1
+
+if [ $1 == "lnx64" ]; then
+
+    g++ *.cpp -o snpldb-$1/snpldb -s -O2 -std=c++11 -static
+
+elif [ $1 == "win32" ]; then
+
+    i686-w64-mingw32-g++ *.cpp -o snpldb-$1/snpldb.exe -s -O2 -std=c++11 -static
+
+elif [ $1 == "win64" ]; then
+
+    x86_64-w64-mingw32-g++ *.cpp -o snpldb-$1/snpldb.exe -s -O2 -std=c++11 -static
+
+fi
